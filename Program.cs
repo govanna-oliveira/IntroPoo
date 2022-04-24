e25fa433e65344f8ffe1c377d517914a8ce403d4
geovanna-oliveira <102254889+govanna-oliveira@users.noreply.github.com>
2022-04-24 19:14:27 -0300

    commit inicial

---
+++ Program.cs
@@ -0,0 +1,15 @@
+Quadrado q1 = new Quadrado(10.0);
+
+Console.WriteLine(q1.Lado);
+
+q1.Lado = 8.0;
+
+Console.WriteLine(q1.Area);
+Console.WriteLine(q1.Perimetro);
+
+
+Retangulo r1 = new Retangulo(10.0,2.0);
+Console.WriteLine(r1.Base);
+Console.WriteLine(r1.Altura);
+Console.WriteLine(r1.Area);
+Console.WriteLine(r1.Perimetro);
